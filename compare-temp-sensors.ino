9d3b11ecd82c44d2a7a262c1a596e3b431737894
spageektti <git@spageektti.cc>
2024-07-30 21:46:18 +0200

    Update compare-temp-sensors.ino

--- compare-temp-sensors.ino
+++ compare-temp-sensors.ino
@@ -1,33 +1,59 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+// Pin definitions
 #define DHT_PIN A0
 #define DHT_TYPE DHT22
+#define ONE_WIRE_BUS 15
+#define THERMISTOR_PIN A1
 
+// DHT setup
 DHT dht(DHT_PIN, DHT_TYPE);
 
-float thermistorValtoCelsius(int thermistorVal, float BETA){
+// OneWire and DS18B20 setup
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float thermistorValtoCelsius(int thermistorVal, float BETA) {
   return 1 / (log(1 / (1023.0 / thermistorVal - 1)) / BETA + 1.0 / 298.15) - 273.15;
 }
 
 void setup() {
+  // Initialize Serial1 communication
   Serial1.begin(115200);
   Serial1.println("Hello, Raspberry Pi Pico W!");
+
+  // Initialize DHT sensor
   dht.begin();
+
+  // Initialize DS18B20 sensor
+  pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
+  sensors.begin();
 }
 
 void loop() {
-  const float BETA = 3950; // should match the Beta Coefficient of the thermistor
-  int thermistorVal = analogRead(A1);
+  // Thermistor reading
+  const float BETA = 3950; // Beta coefficient of the thermistor
+  int thermistorVal = analogRead(THERMISTOR_PIN);
   float thermistorCelsius = thermistorValtoCelsius(thermistorVal, BETA);
 
   Serial1.print("Thermistor: ");
   Serial1.println(thermistorCelsius);
 
-  float DHTCelsius = dht.readTemperature();
+  // DHT22 reading
+  float dhtCelsius = dht.readTemperature();
   Serial1.print("DHT22: ");
-  Serial1.println(DHTCelsius);
+  Serial1.println(dhtCelsius);
 
+  // DS18B20 reading
+  sensors.requestTemperatures();
+  float ds18b20Celsius = sensors.getTempCByIndex(0);
+  Serial1.print("DS18B20: ");
+  Serial1.println(ds18b20Celsius);
+
+  // Delay before next reading
   delay(2000); // 2 seconds delay between readings
 }
