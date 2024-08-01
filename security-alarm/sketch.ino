2f1f9c1bc81f7ba173ac503a70870e4729ca0b24
spageektti <git@spageektti.cc>
2024-08-01 13:52:26 +0200

    Update sketch.ino

--- security-alarm/sketch.ino
+++ security-alarm/sketch.ino
@@ -6,7 +6,8 @@
 #define ROTATION_LEFT 90
 #define ROTATION_RIGHT 90
 #define DISTANCE = 100 // if distance lower than this the alarm will start
-#define MOTOR_DELAY 15 // miliseconds
+#define MOTOR_DELAY 20 // miliseconds
+#define MOTOR_STEP 10
 
 #include <Servo.h>
 Servo servo;
@@ -16,6 +17,9 @@ int position = ROTATION_LEFT;
 void checkPIRSensor(){
   bool motionDetected = digitalRead(MOTION_SENSOR_PIN);
   Serial1.println(motionDetected);
+}
+void checkDistanceSensor(){
+  
 }
 void setup() {
   // put your setup code here, to run once:
@@ -29,18 +33,22 @@ void setup() {
   servo.attach(MOTOR_PIN);
 }
 void loop() {
-  for(position = ROTATION_LEFT; position >= 0; position--){
+  checkPIRSensor();
+  for(position = ROTATION_LEFT; position >= 0; position -= MOTOR_STEP){
+    checkDistanceSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
   }
-  for(position; position <= ROTATION_LEFT + ROTATION_RIGHT; position++){
+  for(position; position <= ROTATION_LEFT + ROTATION_RIGHT; position += MOTOR_STEP){
+    checkDistanceSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
   }
-  for(position; position >= ROTATION_LEFT; position--){
+  for(position; position >= ROTATION_LEFT; position -= MOTOR_STEP){
+    checkDistanceSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
   }
-  
+  checkPIRSensor();
   delay(1000);
 }
