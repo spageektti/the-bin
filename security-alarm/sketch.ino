f5a9b24ef57f79c14838e448d546f41ccdfdcc0b
spageektti <git@spageektti.cc>
2024-08-01 15:00:52 +0200

    Update sketch.ino

--- security-alarm/sketch.ino
+++ security-alarm/sketch.ino
@@ -1,17 +1,39 @@
+#include <Servo.h>
+#include <Keypad.h>
+
 #define MOTION_SENSOR_PIN 22
 #define MOTOR_PIN 15
 #define TRIG_PIN 3
 #define ECHO_PIN 2
+#define BUZZER_PIN 26
+
 #define ROTATION_LEFT 90
 #define ROTATION_RIGHT 90
-#define DISTANCE 100 // if distance is lower than this the alarm will start
+#define DISTANCE 100 // if distance lower than this the alarm will start
 #define MOTOR_DELAY 20 // milliseconds
 #define MOTOR_STEP 10
+#define TIME_BETWEEN_SCANS 1000
 
-#include <Servo.h>
 Servo servo;
-
 int position = ROTATION_LEFT;
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {6, 7, 8, 9}; 
+byte colPins[COLS] = {21, 20, 19, 18}; 
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+String inputPIN;
+const String correctPIN = "1A2B3C4D"; // very secure PIN btw
+bool alarmActive = false;
 
 void checkPIRSensor(){
   bool motionDetected = digitalRead(MOTION_SENSOR_PIN);
@@ -19,6 +41,7 @@ void checkPIRSensor(){
   Serial1.println(motionDetected);
   if(motionDetected){
     Serial1.println("Alarm!");
+    alarmActive = true;
   }
 }
 
@@ -42,38 +65,72 @@ void checkDistanceSensor(){
 
   if (distance < DISTANCE) {
     Serial1.println("Alarm!");
+    alarmActive = true;
   }
 }
 
+void triggerAlarm(){
+  while(alarmActive){
+    digitalWrite(BUZZER_PIN, HIGH);
+    char key = keypad.getKey();
+    if (key){
+      Serial1.println(key);
+      if (key == '#') {
+        if (inputPIN == correctPIN) {
+          alarmActive = false;
+          inputPIN = "";
+          Serial1.println("Alarm deactivated.");
+        } else {
+          inputPIN = "";
+          Serial1.println("Incorrect PIN. Try again.");
+        }
+      } else {
+        inputPIN += key;
+      }
+    }
+  }
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
 void setup() {
-  // put your setup code here, to run once:
   Serial1.begin(115200);
   Serial1.println("Hello, Raspberry Pi Pico W!");
 
   pinMode(MOTION_SENSOR_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
   servo.attach(MOTOR_PIN);
 }
 
 void loop() {
-  checkPIRSensor();
   for(position = ROTATION_LEFT; position >= 0; position -= MOTOR_STEP){
     checkDistanceSensor();
+    checkPIRSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
+     if (alarmActive) {
+      triggerAlarm();
+    }
   }
   for(position = 0; position <= ROTATION_LEFT + ROTATION_RIGHT; position += MOTOR_STEP){
     checkDistanceSensor();
+    checkPIRSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
+     if (alarmActive) {
+      triggerAlarm();
+    }
   }
   for(position = ROTATION_LEFT + ROTATION_RIGHT; position >= ROTATION_LEFT; position -= MOTOR_STEP){
     checkDistanceSensor();
+    checkPIRSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
+     if (alarmActive) {
+      triggerAlarm();
+    }
   }
-  checkPIRSensor();
-  delay(1000);
+  delay(TIME_BETWEEN_SCANS);
 }
