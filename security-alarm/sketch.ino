98585c662fc1e646ce278c0b2d0ee387f2e5a4fc
spageektti <git@spageektti.cc>
2024-08-01 14:36:15 +0200

    Update sketch.ino

--- security-alarm/sketch.ino
+++ security-alarm/sketch.ino
@@ -1,12 +1,11 @@
-// Detect motion using PIR sensor and Ultrasonic Distance Sensor. The Ultrasonic one can rotate 360 or 180 degrees.
 #define MOTION_SENSOR_PIN 22
 #define MOTOR_PIN 15
 #define TRIG_PIN 3
 #define ECHO_PIN 2
 #define ROTATION_LEFT 90
 #define ROTATION_RIGHT 90
-#define DISTANCE = 100 // if distance lower than this the alarm will start
-#define MOTOR_DELAY 20 // miliseconds
+#define DISTANCE 100 // if distance is lower than this the alarm will start
+#define MOTOR_DELAY 20 // milliseconds
 #define MOTOR_STEP 10
 
 #include <Servo.h>
@@ -16,11 +15,36 @@ int position = ROTATION_LEFT;
 
 void checkPIRSensor(){
   bool motionDetected = digitalRead(MOTION_SENSOR_PIN);
+  Serial1.print("Motion detected? ");
   Serial1.println(motionDetected);
+  if(motionDetected){
+    Serial1.println("Alarm!");
+  }
 }
+
 void checkDistanceSensor(){
-  
+  long duration;
+  int distance;
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  distance = duration * 0.034 / 2; // speed of sound wave divided by 2 (go and back)
+
+  Serial1.print("Distance: ");
+  Serial1.println(distance);
+
+  if (distance < DISTANCE) {
+    Serial1.println("Alarm!");
+  }
 }
+
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(115200);
@@ -32,6 +56,7 @@ void setup() {
 
   servo.attach(MOTOR_PIN);
 }
+
 void loop() {
   checkPIRSensor();
   for(position = ROTATION_LEFT; position >= 0; position -= MOTOR_STEP){
@@ -39,12 +64,12 @@ void loop() {
     servo.write(position);
     delay(MOTOR_DELAY);
   }
-  for(position; position <= ROTATION_LEFT + ROTATION_RIGHT; position += MOTOR_STEP){
+  for(position = 0; position <= ROTATION_LEFT + ROTATION_RIGHT; position += MOTOR_STEP){
     checkDistanceSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
   }
-  for(position; position >= ROTATION_LEFT; position -= MOTOR_STEP){
+  for(position = ROTATION_LEFT + ROTATION_RIGHT; position >= ROTATION_LEFT; position -= MOTOR_STEP){
     checkDistanceSensor();
     servo.write(position);
     delay(MOTOR_DELAY);
