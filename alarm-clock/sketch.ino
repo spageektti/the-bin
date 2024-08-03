30fcec573b2c196feebe6a11124e4ec244461946
spageektti <git@spageektti.cc>
2024-08-03 10:21:13 +0200

    Update sketch.ino

--- alarm-clock/sketch.ino
+++ alarm-clock/sketch.ino
@@ -1,14 +1,24 @@
 #include <IRremote.h>
+#include <TM1637.h>
 
-#define RECEIVER 26
+#define RECEIVER 12
+#define CLK 2
+#define DIO 3
+
+TM1637 tm(CLK, DIO);
 
 int currentDigit = 0;
 int alarmDigits[] = {0, 0, 0, 0};
 bool alarmSet = false;
+bool displayTimeFlag = true;
 
 void setup() {
   Serial1.begin(115200);
+
   IrReceiver.begin(RECEIVER, ENABLE_LED_FEEDBACK);
+
+  tm.init();
+  tm.set(BRIGHT_TYPICAL);
 }
 
 void loop() {
@@ -65,6 +75,12 @@ void loop() {
     }
     IrReceiver.resume(); // Receive the next value
   }
+  if(displayTimeFlag){
+    displayTime();
+  }
+  else{
+    displayOnScreen();
+  }
 }
 
 void stopEditing() {
@@ -74,34 +90,52 @@ void stopEditing() {
     Serial1.print(alarmDigits[i]);
   }
   Serial1.println();
+  displayTimeFlag = true;
 }
 
 void switchToPreviousDigit() {
   currentDigit = (currentDigit - 1 + 4) % 4;
   Serial1.print("Switched to previous digit: ");
   Serial1.println(currentDigit);
+  displayTimeFlag = false;
 }
 
 void switchToNextDigit() {
   currentDigit = (currentDigit + 1) % 4;
   Serial1.print("Switched to next digit: ");
   Serial1.println(currentDigit);
+  displayTimeFlag = false;
 }
 
 void addToDigit() {
   alarmDigits[currentDigit] = (alarmDigits[currentDigit] + 1) % 10;
   Serial1.print("Added to digit: ");
   Serial1.println(alarmDigits[currentDigit]);
+  displayTimeFlag = false;
 }
 
 void substractFromDigit() {
   alarmDigits[currentDigit] = (alarmDigits[currentDigit] - 1 + 10) % 10;
   Serial1.print("Subtracted from digit: ");
   Serial1.println(alarmDigits[currentDigit]);
+  displayTimeFlag = false;
 }
 
 void setDigit(int digit) {
   alarmDigits[currentDigit] = digit;
   Serial1.print("Set digit to: ");
   Serial1.println(digit);
+  displayTimeFlag = false;
+}
+
+void displayOnScreen(){
+  for(int i = 0; i < 4; i++){
+    tm.display(i, alarmDigits[i]);
+  }
+}
+
+void displayTime(){
+  for(int i = 0; i < 4; i++){
+    tm.display(i, alarmDigits[i] + 1);
+  }
 }
